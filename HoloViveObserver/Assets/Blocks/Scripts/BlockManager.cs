d919f27b9d0e68f069376a7ec21784b9e80c16ba
aimlessPolymath <solomon968@gmail.com>
2017-02-05 09:34:34 -0500

    Multiple changes to block creation, including optimization features:minimum distance, and stretching cubes as needed.

--- HoloViveObserver/Assets/Blocks/Scripts/BlockManager.cs
+++ HoloViveObserver/Assets/Blocks/Scripts/BlockManager.cs
@@ -12,21 +12,68 @@ public class BlockManager : NetworkBehaviour
     private bool placingCube = false;
     private GameObject currentCube = null;
 
+    private Vector3 oldLeftPosition;
+    private Vector3 oldRightPosition;
+    private float minimumDistance = 0.2f;
+
     public override void OnStartClient()
     {
         base.OnStartClient();
 
+        /*
         leftController.TriggerClicked += TriggerClicked;
         rightController.TriggerClicked += TriggerClicked;
         leftController.TriggerUnclicked += TriggerUnclicked;
         rightController.TriggerUnclicked += TriggerUnclicked;
+        */
+        //Using the touchpad instead
+        //Using the touchpad instead
+        leftController.PadClicked += TriggerClicked;
+        rightController.PadClicked += TriggerClicked;
+        leftController.PadUnclicked += TriggerUnclicked;
+        rightController.PadUnclicked += TriggerUnclicked;
+        
+        oldLeftPosition = leftController.transform.position;
+        oldRightPosition = rightController.transform.position;
+
     }
-    
+
+        private Vector3 leftMovement()
+    {
+        return leftController.transform.position - oldLeftPosition;
+    }
+
+    private float leftDistance()
+    {
+        return leftMovement().magnitude;
+    }
+
+    private Quaternion leftDirection()
+    {
+        return Quaternion.LookRotation(leftMovement(), Vector3.up);
+    }
+
+    private Vector3 rightMovement()
+    {
+        return rightController.transform.position - oldRightPosition;
+    }
+    private float rightDistance()
+    {
+        return rightMovement().magnitude;
+    }
+
+    private Quaternion rightDirection()
+    {
+        return Quaternion.LookRotation(rightMovement(), Vector3.up);
+    }
+
+
     void Update()
     {
-        if (isClient && placingCube)
+        if (isClient && placingCube && leftDistance() > minimumDistance)
+
         {
-			CmdPlaceCube();
+            CmdPlaceCube();
         }
     }
 
@@ -77,6 +124,8 @@ public class BlockManager : NetworkBehaviour
     {
         //currentCube.GetComponent<BlockController>().FinishPlacing();
         currentCube = null;
+        oldLeftPosition = leftController.transform.position;
+
 
         placingCube = false;
     }
@@ -84,16 +133,22 @@ public class BlockManager : NetworkBehaviour
     [ClientCallback]
     private void UpdateCubePosition()
     {
+        Quaternion leftAim = leftDirection();
+        float length = leftDistance();
+
+
         Vector3 left = leftController.transform.position;
         //Vector3 right = rightController.transform.position;
-        CmdUpdateCubePosition(left);
+        CmdUpdateCubePosition(left, leftAim, leftDistance());
     }
 
     [Command]
-    private void CmdUpdateCubePosition(Vector3 position)
+    private void CmdUpdateCubePosition(Vector3 position, Quaternion newDirection, float distance)
     {
         if (!currentCube) return;
         currentCube.transform.position = position;
-		//currentCube.transform.localScale = new Vector3(0.01f,0.01f,0.01f);
+        currentCube.transform.localRotation = newDirection;
+        currentCube.transform.localScale = new Vector3(0.01f, distance, 0.01f);
+
     }
 }
